aac8c3c26850cb094df7dd7a0f3d9bbbaa3e9b3d
Victor Fernandes <criw@pm.me>
2019-02-04 09:47:26 +0100

    added: initial commit

---
+++ init.el
@@ -0,0 +1,43 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;; Set up some package respositories.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; Test use-package to be installed, otherwise installs it.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file
+ (expand-file-name
+  (concat user-emacs-directory "/conf/conf.org")))
+
+(eval-when-compile
+  (require 'use-package))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7f89ec3c988c398b88f7304a75ed225eaac64efa8df3638c815acc563dfd3b55" "9b65cf71fd6b27a5362afeff062c6abd1c5d8a7c4d444c942f3da36bf0a151b1" default)))
+ '(package-selected-packages
+   (quote
+    (web-mode ledger projectile 0blayout org-bullets editorconfig company markdown-mode cargo htmlize use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
